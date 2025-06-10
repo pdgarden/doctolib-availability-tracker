4a8d961f0c560a7881bdf04ffb3b2ca6b0781d4b
JB Bailly <jean.baptiste.bailly@gmail.com>
2025-06-11 00:30:45 +0200

    feat: enhances email notifications with HTML support
    Updates email sending to support both plain text and HTML formats.
    
    Also, configures SMTP server to be configurable through environment variables.

--- src/notify.py
+++ src/notify.py
@@ -1,18 +1,34 @@
 import smtplib
 import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from loguru import logger
 
 from src.configuration import config
 
 
-def send_email(receiver_email: str, message: str) -> None:
-    """Send email using gmail."""
+def send_email(receiver_email: str, subject: str, message_txt: str, message_html: str) -> None:
+    """Send email using SMTP."""
     port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
+    smtp_server = config.smtp_server
+
+    # Create the email
+    msg = MIMEMultipart("alternative")
+    msg["From"] = config.sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = subject
+
+    # Attach the message to the email
+    msg.attach(MIMEText(message_txt, "plain"))
+    msg.attach(MIMEText(message_html, "html"))
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(config.sender_email, config.sender_gmail_password.get_secret_value())
-        server.sendmail(config.sender_email, receiver_email, message)
+        server.sendmail(config.sender_email, receiver_email, msg.as_string())
     logger.debug(f"Email sent to {receiver_email}")
+
+
+if __name__ == "__main__":
+    send_email(config.receiver_email, "Test", "This is a test email.")
